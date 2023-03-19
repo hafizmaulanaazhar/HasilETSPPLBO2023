b1133306aeb1ed3a016af8268d974ec1bf6b7f4c
hafizmaulanaazhar <77614062+hafizmaulanaazhar@users.noreply.github.com>
2023-03-19 11:39:13 +0700

    hapus directory

--- caffeMain/orderServices/orderServices/src/main/java/com/etsCaffe/orderServices/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etsCaffe.orderServices.repository;
-
-import com.etsCaffe.orderServices.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- caffeMain/productServices/productServices/src/main/java/com/etsCaffe/productServices/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etsCaffe.productServices.repository;
-
-import com.etsCaffe.productServices.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
